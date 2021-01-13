3b827b09894f06913ac2c223c1c59e4e11036d3b
Luiz Freitas <luiz_dev@outlook.com>
2021-01-13 14:21:56 -0300

    [ADD] - inicial route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :jobs
   resources :candidates
+  get "/" => "jobs#index"
   get "/candidates_by_city/:id" => "jobs#find_candidates_by_city"
   get "/candidates_by_experience/:id" => "jobs#find_candidates_by_experience"
   get "/candidates_by_technologies/:id" => "jobs#find_candidates_by_technologies"
